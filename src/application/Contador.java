8bf642947709a22f28c19d56bd04eba80e993d20
camara.vitor@hotmail.com <camara.vitor@hotmail.com>
2023-09-16 13:19:22 -0300

    Commit do projeto da DIO de contagem

---
+++ src/application/Contador.java
@@ -0,0 +1,39 @@
+package application;
+
+
+import java.util.Scanner;
+import java.text.ParseException;
+
+public class Contador {
+
+	public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in);
+		System.out.print("Digite o primeiro parâmetro: ");
+		int parametroUm = terminal.nextInt();
+		System.out.print("Digite o segundo parâmetro: ");
+		int parametroDois = terminal.nextInt();
+		
+		try {
+			contar(parametroUm, parametroDois);			
+		}
+		catch (ParametrosInvalidosException e) {
+			System.out.println(e.getMessage());				
+			
+		}	
+		
+		terminal.close();		
+	}
+	
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		if(parametroUm >= parametroDois) {
+			throw new ParametrosInvalidosException("O segundo número não pode ser menor que o primeiro");
+		}
+		int contagem = parametroDois - parametroUm;
+		
+		for(int i = 1; i <= contagem; i++) {
+			System.out.println("imprimindo numero: " + i);			
+			
+		}
+	}
+
+}
---
+++ src/application/ParametrosInvalidosException.java
@@ -0,0 +1,9 @@
+package application;
+
+class ParametrosInvalidosException extends Exception {	
+	private static final long serialVersionUID = 7219276301765152241L;
+     public ParametrosInvalidosException(String message) {
+		super(message);
+	}
+}
+
